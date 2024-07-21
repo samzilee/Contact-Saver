874b0420fb6c3de9c04c77338ee7d415bca811c3
Your Name <you@example.com>
2024-07-21 01:44:45 +0100

    version 1.2

---
+++ config.js
@@ -0,0 +1,46 @@
+let contactList = document.querySelector(".contactList");
+let savedContacts = JSON.parse(localStorage.getItem("contacts"));
+
+const renderContacts = () => {
+  savedContacts = JSON.parse(localStorage.getItem("contacts"));
+  contactList.innerHTML = "";
+  savedContacts.map((contact) => {
+    contactList.insertAdjacentHTML(
+      "beforeend",
+      `<div id="contactjs">
+              <div>
+               <img src="img/icons8-contact-32.png" />
+              </div>
+              <div>
+                <p class="name">${contact.name}</p>
+                <p class="number">${contact.phone}</p>
+              </div>
+            </div>`
+    );
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => renderContacts());
+//function mappingContact
+
+const numberInput = document.getElementById("numberInput");
+const nameInput = document.getElementById("nameInput");
+
+function saveContact() {
+  const phone = numberInput.value;
+  const name = nameInput.value;
+
+  let newContact = { name, phone };
+
+  if (phone === "" || name === "") {
+    let redZone = document.getElementById("redZone");
+    redZone.innerHTML = "input Name or Phone Number";
+    redZone.style.color = "red";
+  } else {
+    savedContacts = [...savedContacts, newContact];
+    console.log(savedContacts);
+    localStorage.setItem("contacts", JSON.stringify(savedContacts));
+    renderContacts();
+    return (numberInput.value = ""), (nameInput.value = "");
+  }
+}
